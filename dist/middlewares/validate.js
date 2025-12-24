2a03d92f4513c26d6c402d6e36004d3f6435a1dd
profvalmeida <vinicius.almeida@prof.faeterj-brm.faetec.rj.gov.br>
2025-12-23 22:27:04 -0300

    Server Alterado

--- dist/middlewares/validate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validate = void 0;
-const validate = (schema) => (req, res, next) => {
-    schema.parse(req.body);
-    next();
-};
-exports.validate = validate;
--- dist/util/somar.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.somar = somar;
-function somar(n1, n2) {
-    return n1 + n2;
-}
